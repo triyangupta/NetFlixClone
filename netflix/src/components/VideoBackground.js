d3e668aeb869a0870f9079734e4110c7ae1e26fe
triyangupta <guptatriyan7@gmail.com>
2026-06-15 11:30:01 +0530

    Fix trailer fallback and video loading issue

--- netflix/src/components/VideoBackground.js
+++ netflix/src/components/VideoBackground.js
@@ -1,18 +1,27 @@
 import useMovieById from "../hooks/useMovieById";
 import { useSelector } from "react-redux";
+import { video_background } from "../utils/constant";
 
 const VideoBackground = ({ movieId, isModal }) => {
-    const trailerMovie = useSelector((store) => store.movie.trailerMovie);
+    const trailerMovie = useSelector(
+        (store) => store.movie.trailerMovie
+    );
 
     useMovieById(movieId);
 
-    if (!trailerMovie?.key) return null;
+    const iframeClass = isModal
+        ? "w-full h-full"
+        : "w-screen aspect-video";
+
+    const videoSrc = trailerMovie?.key
+        ? `https://www.youtube.com/embed/${trailerMovie.key}?autoplay=1&mute=1`
+        : `${video_background}&autoplay=1&mute=1`;
 
     return (
         <iframe
-            className={isModal ? "w-full h-full" : "w-screen aspect-video"}
-            src={`https://www.youtube.com/embed/${trailerMovie.key}?autoplay=1&mute=1`}
-            title="YouTube player"
+            className={iframeClass}
+            src={videoSrc}
+            title="Video Player"
             allow="autoplay; encrypted-media; picture-in-picture"
             allowFullScreen
         />
@@ -20,6 +29,3 @@ const VideoBackground = ({ movieId, isModal }) => {
 };
 
 export default VideoBackground;
-
-// src={`https://www.youtube.com/embed/${trailerMovie?.key}?si=qscP6VvxddB5rs0m&autoplay=1&mute=1`} //----Trailer MOvie
-// src={`${video_background}&autoplay=1&mute=1`}  // ----youTube Video
